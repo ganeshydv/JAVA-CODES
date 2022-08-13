f77b96593d596f866acc953ca2b70b1c0c6f289e
Ganesh Gurkhude <97801118+ganeshydv@users.noreply.github.com>
2022-08-13 10:10:43 +0530

    updated question

--- Array/Array_3_codingbat/Max_Mirror.java
+++ Array/Array_3_codingbat/Max_Mirror.java
@@ -1,55 +1,54 @@
+//
+//We'll say that a "mirror" section in an array is a group of contiguous elements such that somewhere in the array,
+//the same group appears in reverse order. For example, the largest mirror section in
+//{1, 2, 3, 8, 9, 3, 2, 1} is length 3 (the {1, 2, 3} part). Return the size of the largest mirror section found in the given array.
+
+
+//maxMirror([1, 2, 3, 8, 9, 3, 2, 1]) → 3
+//maxMirror([1, 2, 1, 4]) → 3
+//maxMirror([7, 1, 2, 9, 7, 2, 1]) → 2
+
 public class Max_Mirror {
     public static void main(String[] args) {
         int[] arr={1, 2, 1, 20, 21, 1, 2, 1, 2, 23, 24, 2, 1, 2, 1, 25};
         int a=maxMirror(arr);
         System.out.println(a);
     }
-
-    static int maxMirror(int[] nums) {
-        int count=0;
-        int count_2=0;
-        int max=0;
-        int j=0;
-        int index1=nums.length-1;
-
-        for(int i=0;i<nums.length;i++)
-        {   //---------- if same element continuous repeating----------
-            if(i-1>=0 &&nums[i]==nums[i-1]){
-                count_2++;
-                if(i+1<nums.length && (nums[i]==nums[i+1])){
-                    count_2++;
-                }
-            }else count_2=0;
-
-            //---------------for comparing----------------
-            for(j=index1;j>=0;j--)
-            {
-
-                if(nums[i]==nums[j] && i +1< nums.length && j-1>=0 && nums[i+1]==nums[j-1])
-                { count++;
-                    index1=j-1;
-                    break;
-                }else if(nums[i]==nums[j] && j+1< nums.length && i-1>=0 && nums[i-1]==nums[j+1] )
-                {
-                    count++;
-                    index1=j-1;
-                    break;
-                }else count=0;
-
-            }
-            //------------------------block over---------------
-            if(count>max)
-            {
-                max=count;
-            }
-            if(count_2>max){
-                max=count_2;
-            }
-
+public int maxMirror(int[] nums) {
+  int count=0;
+  int count2=0;
+  int max=0;
+ 
+  int index1=nums.length-1;
+  if(nums.length>=1) max=1;
+  for(int i=0;i<nums.length;i++)
+  {
+    for(int k=index1;k>=i;k--)
+    {
+      int j=i;
+      if(nums[j]==nums[k])
+      {
+        count=1;
+        while(j+1<nums.length && k-1>=0 && nums[j]==nums[k] && nums[j+1]==nums[k-1])
+        { 
+          count++;
+          k--;
+          j++;
         }
+        
+      }//if end
+      if(count>max)
+      {
+      max=count;
+      }
+    }//inner for
+      
+  }//outer for
+    
+  return max;
+}
 
-        return max;
-    }
+
 
 }
 
