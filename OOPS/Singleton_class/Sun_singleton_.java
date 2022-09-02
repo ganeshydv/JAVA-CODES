39ac703b19b18796b01f88c42ca09664151d57ca
Ganesh Gurkhude <97801118+ganeshydv@users.noreply.github.com>
2022-09-02 16:47:55 +0530

    updated code

--- OOPS/Singleton_class/Sun_singleton_.java
+++ OOPS/Singleton_class/Sun_singleton_.java
@@ -4,6 +4,9 @@ package Singleton_class;
 
 public class Sun_singleton_ {
     private static Sun_singleton_ ref;
+    private Sun_singleton_(){
+
+    }
     public static Sun_singleton_ getSunSingleton(){
         if(ref ==null){
             ref=new Sun_singleton_();
@@ -11,12 +14,14 @@ public class Sun_singleton_ {
         return ref;
     }
 
+}
+class Acces{
     public static void main(String[] args) {
-        Sun_singleton_ s1=new Sun_singleton_();
-        Sun_singleton_ s2= new Sun_singleton_();
-        System.out.println(s1==s2);
-        Sun_singleton_ s3=getSunSingleton();
-        Sun_singleton_ s4=getSunSingleton();
+//        Sun_singleton_ s1=new Sun_singleton_();
+//        Sun_singleton_ s2= new Sun_singleton_();
+//        System.out.println(s1==s2);
+        Sun_singleton_ s3=Sun_singleton_.getSunSingleton();
+        Sun_singleton_ s4=Sun_singleton_.getSunSingleton();
         System.out.println(s3==s4);
     }
 }
