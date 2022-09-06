e4a2b42fd0690785f9453923920d1a256c8bf1f8
Ganesh Gurkhude <97801118+ganeshydv@users.noreply.github.com>
2022-09-06 23:16:30 +0530

    Updated code

--- OOPS/blocks_/Mainclass.java
+++ OOPS/blocks_/Mainclass.java
@@ -2,6 +2,9 @@ package blocks_;
 
 public class Mainclass {
     public static void main(String[] args) {
-        System.out.println(Block_init_var.a);
+        System.out.println("in main() ...static  a="+Block_init_var.a);
+        Block_init_var ref=new Block_init_var();
+        System.out.println("in main()  non static b="+ref.b);
+
     }
 }
