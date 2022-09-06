1a3d0b55a86b96b9d8f119161f7d1c7417943299
Ganesh Gurkhude <97801118+ganeshydv@users.noreply.github.com>
2022-09-06 23:15:54 +0530

    updated code

--- OOPS/blocks_/Block_init_var.java
+++ OOPS/blocks_/Block_init_var.java
@@ -2,7 +2,13 @@ package blocks_;
 
 public class Block_init_var {
     static int a=20;
+    int b=20;
     static {
+        System.out.println("in static block : a="+a);
         a=10;
+    }
+    {
+        System.out.println("in non static block"+b);
+        b=30;
     }
 }
